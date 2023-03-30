e44b61a1b5d40f90a3a42a062e6d346d21ee62d5
aysllanferreira <aysllan.ferreiraks@gmail.com>
2023-03-30 01:02:21 -0300

    testnew

--- src/index.js
+++ src/index.js
@@ -3,9 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+function renderApp(gl, session) {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+      <App gl={gl} session={session} />
+    </React.StrictMode>,
+  );
+}
+
+if (!window.ReactAppAR) {
+  window.ReactAppAR = {};
+}
+window.ReactAppAR.startAR = renderApp;
+
+if (!window.location.pathname.startsWith('/ar')) {
+  renderApp();
+}
